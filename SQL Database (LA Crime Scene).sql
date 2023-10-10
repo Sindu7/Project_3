04de201fe9170b0a8195cc0840e8a681df1b9e21
Pasin <89669387+Sindu7@users.noreply.github.com>
2023-10-09 20:57:49 -0700

    day 1

---
+++ SQL Database (LA Crime Scene).sql
@@ -0,0 +1,23 @@
+CREATE TABLE "LAHomeless" (
+    "ReportID" int,
+    "Report Type" varchar(10),
+    "Year" int,
+    "Arrest Date" date,
+    "Time" int,
+    "Time Range" varchar(255),
+    "Area Name" varchar(255),
+    "Reporting District" int,
+    "Age" int,
+    "Sex" varchar(255),
+    "Race" varchar(255),
+    "Charge Group Description" varchar(1000),
+    "Arrest Type" varchar(255),
+    "Charge" varchar(255),
+    "Charge Description" varchar(1000),
+    "Disposition Description" varchar(1000),
+    "Address" varchar(1000),
+    "Cross Street" varchar(100),
+    "LAT" float,
+    "LON" float,
+    "CD" int
+);
