70068b5e475610daa0905a063af880c7b1c5ce8a
kylemyat <kyle.myatg@gmail.com>
2023-10-15 14:15:27 -0700

    fill in

--- logic.js
+++ logic.js
@@ -1,4 +1,25 @@
+//Fill in the blank once API is available
 
+//let url = 
+
+// Perform a GET request to the query URL/
+//d3.json(url).then(function (data) {
+
+//    console.log(data)
+    // Once we get a response, send the data.features object to the createFeatures function.
+//  createFeatures(data.features);
+//});
+
+//function createMarker(){
+//    let markers = {
+//        radius: ,
+//        fillColor: chooseColor(),
+//        color: chooseColor(),
+//        fillOpacity: 0.5,
+//        weight: 0.5
+//    }
+//    return L.circleMarker();
+//};
 
 
 let myMap = L.map("map", {
